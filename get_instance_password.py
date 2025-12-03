8b58b0afb15ca8dcdeb93034c96136055536e441
Rene Luria <rene.luria@infomaniak.com>
2025-12-03 10:19:58 +0100

    chore: update year in copyright

--- get_instance_password.py
+++ get_instance_password.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2024 demo-opensense contributors
+# Copyright (C) 2025 demo-opensense contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
